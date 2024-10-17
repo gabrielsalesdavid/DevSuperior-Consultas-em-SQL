d7244a9f8284f87e151dc9b53dea64c26e36f8f1
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-17 11:06:35 -0300

    Projeção e Restrição

--- Exercicio-DQL-Exemplo.sql
+++ Exercicio-DQL-Exemplo.sql
@@ -1,4 +1,14 @@
 /* DQL */
 
+/* Projeção em SQL*/
+
+/* Traz em BD apenas os conceitos atribuidos, como: id e nome */
+
 SELECT id, nome
 FROM tb_empregado;
+
+/* Restrição em SQL */
+
+/* Traz em BD apenas o conceito atribuido a ele, como: todos que estejam neste id */
+
+SELECT * FROM tb_empregado WHERE dept_id = 2;
