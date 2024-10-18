8720025ef17e1f96dda100a2022cc4603564c04f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-18 14:14:15 -0300

    Inserting JOINs and Renaming(AS) Removing and Eliminating Ambiquity Aliasing Column Calculations Query(Parent) with a Sub-query(Child)

--- Exercicio-DQL-Exemplo.sql
+++ Exercicio-DQL-Exemplo.sql
@@ -22,11 +22,51 @@ SELECT * FROM tb_empregado, tb_departamento;
 
 /* Faz o cruzamento entre duas tabelas apenas */
 
-SELECT * FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.IDENTITY
+SELECT * FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
 
 /* Tipos de Junção */
 
 /* INNER JOIN Refere dados da duas tabelas*/
 
+SELECT * FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
 /* LEFT JOIN Refere todos os dados da tabela da esquerda e um pouco da direita */
 
+SELECT * FROM tb_empregado LEFT JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* RIGHT JOIN Refere todos os dados da tabela da direita e um pouco da esquerda */
+
+SELECT * FROM tb_empregado RIGHT JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* FULL JOIN refere todos os dados das tabelas */
+
+SELECT * FROM tb_empregado FULL JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* Renomeação (AS) */
+
+/* Remover Ambiquidade */
+
+SELECT * FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* Elimina a Ambiquidade */
+
+SELECT tb_empregado.id, tb_empregado.nome FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* Apelida a coluna da tabela */
+
+SELECT tb_empregado.id, tb_empregado.nome, tb_departamento.nome AS nome_departamento
+FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* Calculos */
+
+SELECT tb_empregado.id, tb_empregado.nome, tb_empregado.id * 10 AS Calculo
+FROM tb_empregado INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id;
+
+/* Uma consulta(pai) com a sub-consulta(filho) */
+
+SELECT dept_id
+FROM (
+    SELECT *
+    FROM tb_empregado
+    INNER JOIN tb_departamento ON tb_empregado.dept_id = tb_departamento.id
+) AS juncao;
