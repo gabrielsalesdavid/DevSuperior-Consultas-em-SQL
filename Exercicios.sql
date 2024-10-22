c4b61d545c639ceee5a98ef7fa5f063147ee5cd9
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-22 10:47:58 -0300

    Exercicio 2604

--- Exercicios.sql
+++ Exercicios.sql
@@ -6,3 +6,6 @@ SELECT name FROM customers WHERE state = 'RS';
 
 SELECT name, street FROM customers WHERE city = 'Porto Alegre';
 
+/* Exercicio 2604 */
+
+SELECT id, name FROM products WHERE price < 10 OR price > 100;
