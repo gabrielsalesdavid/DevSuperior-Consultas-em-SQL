4721c2b3174437d4b3c6d20bd86d45faf6f937f7
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-10-23 11:56:17 -0300

    Exercicio 2606

--- Exercicios.sql
+++ Exercicios.sql
@@ -15,3 +15,8 @@ SELECT id, name FROM products WHERE price < 10 OR price > 100;
 SELECT products.name, providers.name FROM products
 INNER JOIN providers ON providers.id = products.id_providers
 WHERE products.id_categories = 6;
+
+/* Exercicio 2606 */
+
+SELECT products.id, products.name FROM products
+INNER JOIN categories ON categories.id = products.id_categories WHERE categories.name LIKE 'super%';
