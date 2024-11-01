8b50f1c5b95cfd6876905708d5fe263992444456
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-11-01 19:03:56 -0300

    Exercico 2998

--- Exercicios.sql
+++ Exercicios.sql
@@ -498,3 +498,35 @@ FROM empregado
 			GROUP BY empregado.matr) AS t2 ON empregado.matr = t2.matr
 GROUP BY empregado.matr, departamento.nome, empregado.nome, t1.Salario_Bruto, t2.Total_Desconto
 ORDER BY(t1.Salario_Bruto - t2.Total_Desconto) DESC;
+
+/* Exercico 2998 */
+
+WITH running_operations_returns AS (
+SELECT 
+		clients.id,
+		clients.name,
+		clients.investment,
+		operations.month,
+		SUM(profit) OVER (PARTITION BY client_id ORDER BY operations.id) AS running_sum_of_profit,
+		SUM(profit) OVER (PARTITION BY client_id ORDER BY operations.id) - clients.investment AS return
+	FROM operations
+	INNER JOIN clients
+		ON operations.client_id = clients.id
+	ORDER BY operations.client_id, operations.month
+)
+SELECT 
+	payback_month_return.name,
+	running_operations_returns.investment,
+	running_operations_returns.month AS month_of_payback,
+	payback_month_return.return
+FROM (
+	SELECT DISTINCT ON (id)
+		name,
+		investment,
+		return
+	FROM running_operations_returns
+	WHERE return >= 0
+) AS payback_month_return
+INNER JOIN running_operations_returns
+	USING (return )
+ORDER BY return DESC;
